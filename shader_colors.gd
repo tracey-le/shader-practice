e3d74ccc28a818486e764dcfb59565f6f2a8856a
Tracey Le <traceyle@outlook.com>
2026-08-13 15:16:20 -0400

    Initial commit

---
+++ shader_colors.gd
@@ -0,0 +1,14 @@
+extends MeshInstance3D
+@export var colors: PackedColorArray
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var color_shader_material := mesh.surface_get_material(0) as ShaderMaterial
+	color_shader_material.set_shader_parameter("colors", colors)
+	var colors_from_shader = color_shader_material.get_shader_parameter("colors")
+	print(colors_from_shader)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
